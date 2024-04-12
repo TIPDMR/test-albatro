446ef06355106aaa21fb98eec54f8b25320cbd60
Дмитрий Типсин <tipsin.dm@yandex.ru>
2024-04-12 07:40:38 +0300

    chore: Описания метода stackMiddlewares

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,8 +1,6 @@
 import { middlewareNextAuth } from '@app/middlewares/nextAuth';
 import { stackMiddlewares } from '@app/middlewares/stackMiddlewares';
 
-//export { default } from 'next-auth/middleware';
-
 const middlewares = [middlewareNextAuth];
 export default stackMiddlewares(middlewares);
 
